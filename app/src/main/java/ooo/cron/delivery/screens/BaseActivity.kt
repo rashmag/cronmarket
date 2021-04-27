ce3b55b4d4470b58508805f5287a42922b24a001
Ramazan Gadzhikadiev <ramazan.gadjikadiev@gmail.com>
2021-04-27 11:49:43 +0300

    Hide keyboard while out of editText/keyboard is tapped

--- app/src/main/java/ooo/cron/delivery/screens/BaseActivity.kt
+++ app/src/main/java/ooo/cron/delivery/screens/BaseActivity.kt
@@ -1,12 +1,18 @@
 package ooo.cron.delivery.screens
 
-import android.content.Intent
+import android.content.Context
 import android.graphics.Color
+import android.graphics.Rect
 import android.os.Build
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import ooo.cron.delivery.screens.any_error_screen.AnyErrorActivity
 import ooo.cron.delivery.screens.base_mvp.ConnectionErrorActivity
+
 
 /**
  * Created by Ramazan Gadzhikadiev on 13.04.2021.
@@ -25,5 +31,21 @@ abstract class BaseActivity : AppCompatActivity() {
 
     fun showConnectionErrorScreen() {
         ConnectionErrorActivity.show(this)
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (ev?.action == MotionEvent.ACTION_DOWN) {
+            val v: View? = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(ev.rawX.toInt(), ev.rawY.toInt())) {
+                    v.clearFocus()
+                    (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+                        .hideSoftInputFromWindow(v.getWindowToken(), 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }

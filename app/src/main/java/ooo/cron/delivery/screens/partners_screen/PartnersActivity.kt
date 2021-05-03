ecbd14da495c8ee046ee8bbb52b03728b5b93e8a
Muhammad <maga07_1997@mail.ru>
2021-05-03 11:10:52 +0300

    create PartnersActivity.kt

---
+++ app/src/main/java/ooo/cron/delivery/screens/partners_screen/PartnersActivity.kt
@@ -0,0 +1,20 @@
+package ooo.cron.delivery.screens.partners_screen
+
+import android.os.Bundle
+import ooo.cron.delivery.R
+import ooo.cron.delivery.screens.BaseActivity
+
+/*
+ * Created by Muhammad on 02.05.2021
+ */
+
+
+
+class PartnersActivity: BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_partners)
+    }
+}

d5cc409e6740921213c6cd5d912bb01247966012
Muhammad <maga07_1997@mail.ru>
2021-05-30 18:33:21 +0300

    add splash screen (without image)

---
+++ app/src/main/java/ooo/cron/delivery/screens/splash_screen/SplashScreenActivity.kt
@@ -0,0 +1,19 @@
+package ooo.cron.delivery.screens.splash_screen
+
+import android.os.Bundle
+import ooo.cron.delivery.R
+import ooo.cron.delivery.screens.BaseActivity
+
+/*
+ * Created by Muhammad on 30.05.2021
+ */
+
+
+
+class SplashScreenActivity: BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}

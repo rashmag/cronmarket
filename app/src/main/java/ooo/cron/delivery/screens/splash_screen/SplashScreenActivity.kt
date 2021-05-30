2ae670214a00224611b4e2e89707b472ec8de819
Muhammad <maga07_1997@mail.ru>
2021-05-30 20:37:16 +0300

    add splash screen view

--- app/src/main/java/ooo/cron/delivery/screens/splash_screen/SplashScreenActivity.kt
+++ app/src/main/java/ooo/cron/delivery/screens/splash_screen/SplashScreenActivity.kt
@@ -1,8 +1,12 @@
 package ooo.cron.delivery.screens.splash_screen
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import ooo.cron.delivery.R
 import ooo.cron.delivery.screens.BaseActivity
+import ooo.cron.delivery.screens.first_address_selection_screen.FirstAddressSelectionActivity
 
 /*
  * Created by Muhammad on 30.05.2021
@@ -10,10 +14,14 @@ import ooo.cron.delivery.screens.BaseActivity
 
 
 
-class SplashScreenActivity: BaseActivity() {
+class SplashScreenActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, FirstAddressSelectionActivity::class.java))
+        }, 1000)
     }
 }

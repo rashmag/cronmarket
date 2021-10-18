a89dd37278a924af0be204203c0ed391b1d2e945
itsGingerTea <lemarkmain@gmail.com>
2021-10-18 13:41:15 +0300

    CA-2 change margins, corner and background of patrner's tags

--- app/src/main/java/ooo/cron/delivery/data/network/models/TagsResult.kt
+++ app/src/main/java/ooo/cron/delivery/data/network/models/TagsResult.kt
@@ -3,4 +3,7 @@ package ooo.cron.delivery.data.network.models
 /**
  * Created by Ramazan Gadzhikadiev on 22.04.2021.
  */
-data class TagsResult(val tags: List<Tag>, val visibleTagsCount: Int)
+data class TagsResult(
+    val tags: List<Tag>,
+    val visibleTagsCount: Int
+)

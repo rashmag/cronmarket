04623225277aa0db7ad7c5c0f3178a20e45475c1
KamilKhametov <khametovkamil93@gmail.com>
2021-12-20 18:52:04 +0300

    Fixing app crash when location access is denied

--- app/src/main/java/ooo/cron/delivery/screens/first_address_selection_screen/FirstAddressSelectionActivity.kt
+++ app/src/main/java/ooo/cron/delivery/screens/first_address_selection_screen/FirstAddressSelectionActivity.kt
@@ -11,6 +11,7 @@ import android.os.CountDownTimer
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
+import android.widget.Toast
 import androidx.annotation.ColorRes
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.ListPopupWindow
@@ -83,9 +84,16 @@ class FirstAddressSelectionActivity :
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        when (requestCode) {
-            App.LOCATION_PERMISSION_REQUEST_CODE -> presenter.onLocationPermissionGranted()
-            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (permissions.size == 1 &&
+                permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                presenter.onLocationPermissionGranted()
+            } else {
+                Toast.makeText(this, getString(R.string.common_gps_enabled), Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
@@ -420,5 +428,7 @@ class FirstAddressSelectionActivity :
         const val REQUEST_LOCATION_DISTANCE_IN_METERS = 20F
         const val ADDRESS_TYPE_WAITING_IN_MILLIS = 300L
         const val FLAG = "flag"
+
+        const val LOCATION_REQUEST_CODE = 83
     }
 }

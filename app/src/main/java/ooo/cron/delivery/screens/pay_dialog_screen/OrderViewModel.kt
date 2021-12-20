d13c0fa53457f84cdfdfffbaaa9fc30a6431e52c
itsGingerTea <lemarkmain@gmail.com>
2021-12-20 16:46:33 +0300

    add repo

--- app/src/main/java/ooo/cron/delivery/screens/pay_dialog_screen/OrderViewModel.kt
+++ app/src/main/java/ooo/cron/delivery/screens/pay_dialog_screen/OrderViewModel.kt
@@ -5,21 +5,26 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.assisted.AssistedInject
-import ooo.cron.delivery.data.DataManager
+import ooo.cron.delivery.data.OrderPrefsRepository
+import ooo.cron.delivery.data.OrderRestRepository
 
 /**
  * Created by Maya Nasrueva on 14.12.2021
  * */
 
-class OrderViewModel @AssistedInject constructor( private val dataManager: DataManager) : ViewModel() {
+class OrderViewModel @AssistedInject constructor(
+    private val restRepo: OrderRestRepository,
+    private val prefsRepo: OrderPrefsRepository
+) : ViewModel() {
 
     init {
         Log.e("app", "inited")
     }
+
     private val _commentTextLivedata = MutableLiveData("")
     val commentTextLiveData: LiveData<String> = _commentTextLivedata
 
-    fun setComment(comment: String){
+    fun setComment(comment: String) {
         _commentTextLivedata.postValue(comment)
 
     }

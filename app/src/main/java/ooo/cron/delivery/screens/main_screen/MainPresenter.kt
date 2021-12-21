ec3d739c3ba1152c272e3ec3c1b964441cf08db7
KamilKhametov <khametovkamil93@gmail.com>
2021-12-21 15:56:28 +0300

    Correction after MR

--- app/src/main/java/ooo/cron/delivery/screens/main_screen/MainPresenter.kt
+++ app/src/main/java/ooo/cron/delivery/screens/main_screen/MainPresenter.kt
@@ -227,7 +227,10 @@ class MainPresenter @Inject constructor(
     private fun loadSpecialOrders() {
         mainScope.launch {
             try {
-                val specialOffers = dataManager.getSpecialOffers(dataManager.readChosenCityId(), dataManager.readSelectedMarketCategory().id)
+                val specialOffers = dataManager.getSpecialOffers(
+                    dataManager.readChosenCityId(),
+                    dataManager.readSelectedMarketCategory().id
+                )
 
                 if (specialOffers.isEmpty())
                     view?.hideSpecialOffers()

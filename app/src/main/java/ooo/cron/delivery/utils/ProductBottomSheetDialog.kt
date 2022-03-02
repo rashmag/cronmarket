d30c9cf34c4db8937ffc42a3f0caed616a985316
Kamil Abdulatipov <kamil.abdulatipov@skyeng.ru>
2022-03-02 04:01:04 +0300

    resolve conflicts

--- app/src/main/java/ooo/cron/delivery/utils/ProductBottomSheetDialog.kt
+++ app/src/main/java/ooo/cron/delivery/utils/ProductBottomSheetDialog.kt
@@ -133,7 +133,7 @@ class ProductBottomSheetDialog(
                     layoutManager = LinearLayoutManager(context)
                 }
                 val additiveRecyclerAdapter = AdditiveRecyclerAdapter(product.additives)
-                additiveRecyclerAdapter.setListenerAndContext(this@ProductBottomSheetDialog)
+                additiveRecyclerAdapter.setListener(this@ProductBottomSheetDialog)
                 rvAdditives.adapter = additiveRecyclerAdapter
             } else {
                 vgAdditives.visibility = View.GONE

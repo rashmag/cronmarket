a3f8d7024a0bc8dc5660aa83ae93be04f7584ce0
KamilKhametov <khametovkamil93@gmail.com>
2022-04-02 00:24:14 +0300

    Реализация тестового отображения времени доставки + нажатия на кнопки типов времени доставки

---
+++ app/src/main/java/ooo/cron/delivery/screens/pay_dialog_screen/AdapterDeliveryTime.kt
@@ -0,0 +1,77 @@
+package ooo.cron.delivery.screens.pay_dialog_screen
+
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ooo.cron.delivery.R
+import ooo.cron.delivery.databinding.DeliveryTimeItemBinding
+
+class AdapterDeliveryTime : RecyclerView.Adapter<AdapterDeliveryTime.DeliveryTimeViewHolder>() {
+
+    private val deliveryTimes = arrayListOf<String>()
+
+    var parentItem = 1
+    var timeSelected = ""
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = DeliveryTimeViewHolder(
+        itemBinding =  DeliveryTimeItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    )
+
+    override fun onBindViewHolder(holder: DeliveryTimeViewHolder, position: Int) {
+        holder.bind(deliveryTimes[position])
+    }
+
+    fun setTime(list: List<String>){
+        deliveryTimes.run {
+            clear()
+            addAll(list)
+        }
+
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount() = deliveryTimes.size
+
+    inner class DeliveryTimeViewHolder(private val itemBinding: DeliveryTimeItemBinding) : RecyclerView.ViewHolder(itemBinding.root){
+
+        fun bind(model: String){
+
+            with(itemBinding){
+
+                timeText.text = model
+
+                if (parentItem == bindingAdapterPosition) {
+                    timeText.setTextColor(
+                        itemView.context.resources.getColor(
+                            R.color.black
+                        )
+                    )
+
+                    timeSelected = deliveryTimes[bindingAdapterPosition].replace(" ", "")
+
+//                    timeSelectListener?.invoke(array[position])
+
+                    timeText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24f)
+
+                } else if (bindingAdapterPosition == parentItem - 1 || bindingAdapterPosition == parentItem + 1) {
+                    timeText.setTextColor(
+                        itemView.context.resources.getColor(
+                            R.color.grey50
+                        )
+                    )
+                    timeText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f)
+                } else {
+                    timeText.setTextColor(
+                        itemView.context.resources.getColor(
+                            R.color.grey40
+                        )
+                    )
+                    timeText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14f)
+                }
+
+                timeText.text = deliveryTimes[bindingAdapterPosition]
+            }
+        }
+    }
+}
--- app/src/main/java/ooo/cron/delivery/screens/pay_dialog_screen/OrderDeliveryTimeBottomSheet.kt
+++ app/src/main/java/ooo/cron/delivery/screens/pay_dialog_screen/OrderDeliveryTimeBottomSheet.kt
@@ -1,13 +1,21 @@
 package ooo.cron.delivery.screens.pay_dialog_screen
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import ooo.cron.delivery.R
 import ooo.cron.delivery.databinding.PopUpChooseDeliveryTimeBinding
+import ooo.cron.delivery.utils.extensions.makeGone
+import ooo.cron.delivery.utils.extensions.makeVisible
+import ooo.cron.delivery.utils.extensions.orZero
+import ooo.cron.delivery.utils.extensions.uiLazy
 import javax.inject.Inject
 
 class OrderDeliveryTimeBottomSheet : BottomSheetDialogFragment() {
@@ -19,8 +27,14 @@ class OrderDeliveryTimeBottomSheet : BottomSheetDialogFragment() {
         factory.create()
     }
 
+    private val adapterDeliveryTime by uiLazy {
+        AdapterDeliveryTime()
+    }
+
     private var _binding: PopUpChooseDeliveryTimeBinding ?= null
     private val binding get() = _binding!!
+
+    private var deliveryTimeLayoutManager: LinearLayoutManager ?= null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = PopUpChooseDeliveryTimeBinding.inflate(inflater, container, false)
@@ -30,6 +44,68 @@ class OrderDeliveryTimeBottomSheet : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        addClicksForDeliveryTypesBtn()
+        initDeliveryTimeAdapter()
+        initDeliveryTimeRecyclerScrollListener()
+    }
 
+    private fun initDeliveryTimeAdapter(){
+        with(binding.recyclerDeliveryTime) {
+            val timeList = listOf("11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00")
+
+            LinearSnapHelper().attachToRecyclerView(this@with)
+            deliveryTimeLayoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+            layoutManager = deliveryTimeLayoutManager
+            adapter = adapterDeliveryTime
+            adapterDeliveryTime.setTime(timeList)
+        }
+    }
+
+    private fun initDeliveryTimeRecyclerScrollListener(){
+        binding.recyclerDeliveryTime.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                val parentPos = deliveryTimeLayoutManager?.findLastCompletelyVisibleItemPosition()
+
+                if (parentPos != adapterDeliveryTime.parentItem && parentPos != -1) {
+
+                    adapterDeliveryTime.parentItem = parentPos.orZero()
+
+                    adapterDeliveryTime.notifyDataSetChanged()
+                }
+            }
+        })
+    }
+
+    private fun addClicksForDeliveryTypesBtn(){
+        with(binding){
+            btnAsap.apply {
+                setOnClickListener {
+                    setBackgroundResource(R.drawable.bg_market_category_tag_not_selected_item)
+                    setTextColor(Color.parseColor("#FF4C30"))
+
+                    btnByTime.setBackgroundResource(R.drawable.btn_delivery_type_not_selected)
+                    btnByTime.setTextColor(Color.parseColor("#000000"))
+
+                    defaultTimeContainer.makeVisible()
+                    scrollTimeContainer.makeGone()
+                }
+            }
+
+            btnByTime.apply {
+                setOnClickListener {
+                    setBackgroundResource(R.drawable.bg_market_category_tag_not_selected_item)
+                    setTextColor(Color.parseColor("#FF4C30"))
+
+                    btnAsap.setBackgroundResource(R.drawable.btn_delivery_type_not_selected)
+                    btnAsap.setTextColor(Color.parseColor("#000000"))
+
+                    defaultTimeContainer.makeGone()
+                    scrollTimeContainer.makeVisible()
+                }
+            }
+        }
     }
 }
